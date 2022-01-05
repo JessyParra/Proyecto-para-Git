27f45a75393a3faaa44baedb058204d2bc4ceace
Jessy <jessica.parrarincon@gmail.com>
2022-01-05 11:45:26 -0300

    Se creo el proyecto y se añadio el Index y CSS

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+	<meta charset="UTF-8">
+	<link href="CSS/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<header class="contenedor">
+		<div class="logo-contenedor">
+			<img src="IMG/logo.png" width="150px" height="200px"></a>
+			<div class="banner-contenedor">
+			<img src="IMG/banner.jpg" width="750px" height="190px"></a>
+		</div>
+	</header> 
+
+		<div class="contenedor">
+
+			<ul class="menu">	
+				<li><input type="search" name="buscador" placeholder="Buscar...">
+					<input type="submit" value="Buscar"></li>
+					<li></li>
+					<li><a href="#">Inicio</a></li>
+					<li><a href="#">Empresa</a>
+						<ul>
+							<li><a href="#">SubMenu 1</a></li>
+							<li><a href="#">SubMenu 2</a></li>
+							<li><a href="#">SubMenu 3</a></li>
+							<li><a href="#">SubMenu 4</a>
+								<ul>
+									<li><a href="#">Cinco</a></li>
+									<li><a href="#">Seis</a></li>
+									<li><a href="#">Siete</a></li>
+									<li><a href="#">Ocho</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+
+					<li><a href="#">Contacto</a>
+						<ul>
+							<li><a href="#">Teléfono</a></li>
+							<li><a href="#">Correo</a></li>
+							<li><a href="#">Redes Sociales</a>
+								<ul>
+									<li><a href="#">Instagram</a></li>
+									<li><a href="#">Facebook</a></li>
+									<li><a href="#">Twitter</a></li>
+									<li><a href="#">WhatsApp</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<div class="main">
+				<section class="botones">
+					<button class="boton"><a href="usabilidad.php" target="_blank" style="text-decoration:none">Usabilidad</a></button><br><br>
+					<button class="boton"><a href="presencia.php" target="_blank" style="text-decoration:none">Presencia en Internet</a></button><br><br>
+					<button class="boton"><a href="accesibilidad.php" target="_blank" style="text-decoration:none">Accesibilidad</a></button><br><br>
+					<button class="boton"><a href="rapidez.php" target="_blank" style="text-decoration:none">Rapidez de Acceso</a></button><br><br>
+					<button class="boton"><a href="indexacion.php" target="_blank" style="text-decoration:none">Indexación Buscadores</a></button><br><br>
+					<button class="boton"><a href="seguridad.php" target="_blank" style="text-decoration:none">Seguridad</a></button><br><br>
+					<button class="boton"><a href="resultados.php" target="_blank" style="text-decoration:none">Resultados</a>
+				</section>
+			
+			<aside>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed justo placerat, efficitur dui non, euismod nisl. Nunc porta risus erat, ac vulputate dolor ullamcorper id. In ultricies auctor ex, nec ultrices ipsum interdum non. Nulla pulvinar libero at erat pretium, at iaculis nunc vulputate. Morbi vestibulum pulvinar fermentum. Nunc quis nisi tristique, congue ex a, hendrerit nisl. Donec odio turpis, pretium quis ex ac, luctus congue risus. Nullam tempus mi eu justo sodales semper. Etiam porta placerat purus in rhoncus. Nullam varius sit amet odio nec suscipit. Donec pretium enim at lectus iaculis, eu vulputate sem condimentum. Sed sed eleifend ex. In sed mauris id risus congue facilisis vitae quis urna. Aenean non urna iaculis nisi pretium interdum. Quisque non elit auctor, sollicitudin ipsum ac, gravida magna. Vivamus nec magna eros.<br>
+					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus elementum sit amet ligula sit amet hendrerit. Donec enim massa, iaculis vitae elit in, interdum consequat nulla. Mauris elementum viverra dictum. Donec gravida orci sed leo auctor, nec vehicula mi molestie. Nunc mattis bibendum tincidunt. Vestibulum fringilla eu augue ut porta. Nam sed mattis lectus, a euismod eros. Aenean sollicitudin risus a congue faucibus.</p>
+			</aside>
+			</div>
+
+			<footer class="pie contenedor">
+				<p> Página diseñada por <a href="mailto:fatb86@outlook.com">Fabián Toro</a> y <a href="mailto:jessy.696@gmail.com">Jessica Parra</a></p>
+			</footer>  
+
+</body>
+</html>
